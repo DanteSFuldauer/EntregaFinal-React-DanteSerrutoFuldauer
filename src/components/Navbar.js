b19fbf5ad65a812e4b9756b743a4d65cb609ae35
DanteSFuldauer <dantee.fuldauer@gmail.com>
2025-02-22 11:31:53 -0300

    Resuelve conflictos y actualiza archivos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,9 @@ const NavBar = () => {
   return (
     <nav className="container my-5">
       <Link className="navbar-brand d-flex align-items-center" to="/">
-<<<<<<< HEAD
         <img src={logo} alt="Brand Logo" style={{ height: '40px', marginRight: '10px' }}/>BiteTheApple</Link>
-=======
-        <img src={logo} alt="Brand Logo" style={{ height: '40px', marginRight: '10px' }} />
-        BiteTheApple
-      </Link>
->>>>>>> 0c8392a876b31af7648094bbfe85c909f4c5695e
       <div className="is-flex is-align-items-center is-justify-content-center">
-        <NavLink to={`/`} className={({ isActive }) => `button is-primary m-3 ${isActive ? "button is-link m-3" : "button is-primary m-3"}`}>
-          Home
-        </NavLink>
+        <NavLink to={`/`} className={({ isActive }) => `button is-primary m-3 ${isActive ? "button is-link m-3" : "button is-primary m-3"}`}> Home</NavLink>
         <NavLink to={`/category/telefonos`} className={({ isActive }) => `button is-primary m-3 ${isActive ? "button is-link m-3" : "button is-primary m-3"}`}>Teléfonos</NavLink>
         <NavLink to={`/category/notebooks`} className={({ isActive }) => `button is-primary m-3 ${isActive ? "button is-link m-3" : "button is-primary m-3"}`}>Notebooks</NavLink>
         <NavLink to={`/category/auriculares`} className={({ isActive }) => `button is-primary m-3 ${isActive ? "button is-link m-3" : "button is-primary m-3"}`}>Auriculares</NavLink>
